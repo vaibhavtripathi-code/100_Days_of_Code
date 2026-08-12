53058343211bb89335de67bb7a766d93c3b2719b
Vaibhav <vktripathi8810@gmail.com>
2026-08-12 23:57:14 +0530

    Day 9 Completed

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,23 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+bid_data = {}
 
+bid_again = True
+while bid_again:
+    name = input("Please enter your name: ")
+    bid = int(input("Enter your bid amount $: "))
+    bid_data[name] = bid
+    print("\n"*100)
+    ask = input("Does anyone else want to bid? y/n: ").lower()
 
+    if ask == "y" or ask == "yes":
+        continue
+    elif ask == 'n' or ask == "no":
+        bid_again = False
+
+winner = ''
+score = 0
+for i in bid_data:
+    if bid_data[i] > score:
+        score = bid_data[i]
+        winner = i
+
+print(f"The winner is {winner} with a bid of ${score}!")
---
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -0,0 +1,21 @@
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Stuttgart", "Berlin"],
+}
+
+print(travel_log["France"][1])
+
+nested_list = ["A","B","C",["C","D"]]
+print(nested_list[3][1])
+
+travel_log = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+   },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5
+   }}
+
+print(travel_log["Germany"]["cities_visited"][2])
